dc4fcd9eab881bbaf6da241fb74bbb82e3baf454
Nicolas VANDENBOGAERDE <nvandenbogaerde@dotgee.fr>
2014-09-13 10:29:36 +0200

    update version

--- app/models/facture.rb
+++ app/models/facture.rb
@@ -3,7 +3,8 @@ class Facture < ActiveRecord::Base
 	belongs_to :devi
 	accepts_nested_attributes_for :devi	
         has_and_belongs_to_many :pvrecettes
-	has_and_belongs_to_many :bonlivraisons, :delete_sql=>"delete from bonlivraisons_factures where facture_id=#{self.id}"
+	has_and_belongs_to_many :bonlivraisons
+  #:delete_sql=>"delete from bonlivraisons_factures where facture_id=#{self.id}"
 	has_many :avoirs, :dependent => :destroy
 
 	def self.deleteTemporaryFile
