995f1ccbd7fd1ec513f6b117138e31216529baea
Nicolas VANDENBOGAERDE <nvandenbogaerde@dotgee.fr>
2014-09-14 16:18:17 +0200

    update version billings

--- app/models/bonlivraison.rb
+++ app/models/bonlivraison.rb
@@ -1,6 +1,7 @@
 class Bonlivraison < ActiveRecord::Base
 	validates_presence_of :numlivraison, :message => "Numéro du bon de livraison obligatoire"
 	validates_presence_of :devi_id, :message => "Numéro du devi obligatoire"
-	has_and_belongs_to_many :factures, :delete_sql=>"delete from bonlivraisons_factures where bonlivraison_id=#{self.id}"
+	has_and_belongs_to_many :factures
+  #:delete_sql=>"delete from bonlivraisons_factures where bonlivraison_id=#{self.id}"
 	belongs_to :devi
 end
--- app/models/pvrecette.rb
+++ app/models/pvrecette.rb
@@ -3,5 +3,6 @@ class Pvrecette < ActiveRecord::Base
 	validates_presence_of :devi_id, :message => "Séléctionner un devis"
 	belongs_to :devi
 	accepts_nested_attributes_for :devi
-        has_and_belongs_to_many :factures, :delete_sql=>"delete from factures_pvrecettes where pvrecette_id=#{self.id}"	
+  has_and_belongs_to_many :factures
+  #:delete_sql=>"delete from factures_pvrecettes where pvrecette_id=#{self.id}"	
 end
